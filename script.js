b5595b63f5f9d93c089a6eceddd6cf4c998ae13b
jinook <ckffl49@gmail.com>
2023-08-15 14:03:32 +0900

    15일 마지막

---
+++ script.js
@@ -0,0 +1,13 @@
+console. log('hello javascript')
+
+
+var age = 25;
+let year = 2023;
+const point = 250;
+const today = '광복절'; //문자.
+
+const pass = true;
+
+console.log(age);
+console.log(year);
+console.log(250);
